6779e7f22ceb58bb6dd887969828fe9fcca08d87
mizanxali <mizanalip786@gmail.com>
2026-04-04 12:00:00 +0530

    Optimize query in useRegisterDoctorConfirm.js

---
+++ hooks/useRegisterDoctorConfirm.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import { useContractWrite } from "wagmi";
+import useIsDoctor from "@/hooks/useIsDoctor";
+import useValidTxnData from "./useValidTxnData";
+import { getPublicKey } from "@/utils/metamask";
+import useRelayTransaction from "./useRelayTransaction";
+import useStatus from "./useStatus";
+
+export default function useRegisterDoctorConfirm() {
+  const { address } = useValidTxnData();
+  const { relayTransaction, txnLoading, txnWaiting, success } = useRelayTransaction();
+
+  const [publicKey, setPublicKey] = useState(null);
+  const [uploading, setIsUploading] = useState(false);
+
+  const message = useStatus({ uploading, txnLoading, txnWaiting, success });
+
+  useEffect(() => {
+    publicKey &&
+      (async () => {
+        try {
+          console.log(await relayTransaction("registerDrConfirm", [publicKey]));
+        } catch (err) {
+          console.log(err);
+        }
+        setPublicKey(null);
+      })();
+  }, [publicKey]);
+
+  async function registerDrConfirm() {
+    setIsUploading(true);
+    try {
+      setPublicKey(await getPublicKey(address));
+    } catch (err) {
+      console.log(err);
+      setIsUploading(false);
+    }
+    setIsUploading(false);
+  }
+
+  return { status: message, registerDrConfirm };
+}
